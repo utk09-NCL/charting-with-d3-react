a4f4601ba17e22c025d652ad16bfa35d507c21bf
utk09-NCL <utkarsh.t0298@gmail.com>
2025-07-06 13:02:03 +0100

    session 3: d3 mini projects - line chart

---
+++ d3-mini-projects/line-chart/script.js
@@ -0,0 +1,74 @@
+// Chart Configuration
+const margin = { top: 20, right: 20, bottom: 50, left: 50 };
+const width = 800 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+
+let data = [];
+const startDate = new Date("2025-01-01");
+
+// Generate Initial Data
+function generateInitialData() {
+  data = [];
+  for (let i = 0; i < 10; i++) {
+    const date = new Date(startDate);
+    date.setDate(startDate.getDate() + i * 7); // Weekly intervals
+
+    const values = {
+      date: date,
+      value: Math.random() * 80 + 20 + Math.sin(i * 0.5) * 10, // values will be between 20 and 100
+    };
+    data.push(values);
+  }
+}
+
+generateInitialData();
+
+// Create SVG and CHART group
+const svg = d3.select("#line-chart").attr("width", 800).attr("height", 500);
+
+const chartGroup = svg
+  .append("g")
+  .attr("tranform", `translate(${margin.left},${margin.top})`);
+
+// Create scales
+const xScale = d3.scaleTime().range([0, width]);
+
+const yScale = d3.scaleLinear().range([height, 0]);
+
+const line = d3
+  .line()
+  .x((d) => xScale(d.date))
+  .y((d) => yScale(d.value))
+  .curve(d3.curveMonotoneX);
+
+// Create Axes
+const xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%d/%m"));
+const yAxis = d3.axisLeft(yScale);
+
+const xAxisGroup = chartGroup
+  .append("g")
+  .attr("class", "axis x-axis")
+  .attr("transform", `translate(0,${height})`);
+
+const yAxisGroup = chartGroup.append("g").attr("class", "axis y-axis");
+
+// Label on x-axis
+chartGroup
+  .append("text")
+  .attr("class", "axis-label")
+  .attr("x", width / 2)
+  .attr("y", height + 50)
+  .style("text-align", "center")
+  .text("Date");
+
+// Label on y-axis
+chartGroup
+  .append("text")
+  .attr("class", "axis-label")
+  .attr("tranform", "rotate(-90)")
+  .attr("x", -height / 2)
+  .attr("y", -40)
+  .style("text-anchor", "center")
+  .text("Values");
+
+  
