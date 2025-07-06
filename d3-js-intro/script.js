c192f06f486c8688cab851b1be5a0a7429dc033a
utk09-NCL <utkarsh.t0298@gmail.com>
2025-07-06 12:15:04 +0100

    session 3: data joins and transitions

--- d3-js-intro/script.js
+++ d3-js-intro/script.js
@@ -1,7 +1,7 @@
-// select the div with #selection-area
+// select the div with id #selection-area
 d3.select("#selection-area").style("background-color", "yellow");
 
-// select the div with #selection-area and the p with #p1
+// select the div with id #selection-area and the p with id #p1
 d3.select("#selection-area").select("#p1").style("background-color", "yellow");
 
 // select all 'p' tags and style them
@@ -29,3 +29,60 @@ d3.select("#interactive-area").select(":first-child").remove();
 d3.select("#interactive-area")
   .insert("h3", ":last-child")
   .text("Inserted a h3 tag here");
+
+// D3 Joins -- INCOMPLETE, NEED TO ADD JOINS
+const dataJoinArea = d3.select("#data-join-area");
+
+let initialData = [10, 20]; // has 2 data points, so it will create 2 divs
+
+const divs1 = dataJoinArea.selectAll("div").data(initialData);
+
+divs1
+  .enter()
+  .append("div")
+  .text((d) => `INITIAL: ${d}`)
+  .style("background-color", "#0ce444");
+
+let updatedData = [30, 40, 0, 10]; // it will add ONLY 2 more divs (values 0, 10), because 2 divs already exist in the DOM from the initialData
+
+const divs2 = dataJoinArea.selectAll("div").data(updatedData).join();
+
+divs2
+  .enter()
+  .append("div")
+  .text((d) => `ENTER: ${d}`)
+  .style("background-color", "orange");
+
+// Data Transitions
+const transitionArea = d3.select("#transition-area");
+
+function runTransition() {
+  clearTransitions(); // clear out all the html elements inside the selected div
+
+  const mySvg = transitionArea
+    .append("svg")
+    .attr("width", "100%")
+    .attr("height", 500);
+
+  mySvg
+    .selectAll("cicle")
+    .data([20, 40, 60, 80, 100])
+    .enter()
+    .append("circle")
+    .attr("cx", 0)
+    .attr("cy", (d, i) => 25 + i * 20)
+    .attr("r", 10)
+    .style("fill", "#9f2ec2")
+    .transition()
+    .duration(1500)
+    .attr("cx", (d) => d * 3)
+    .attr("cy", (d) => d * 3);
+}
+
+runTransition();
+
+function clearTransitions() {
+  transitionArea.html("");
+}
+
+// clearTransitions(); // Uncomment to clear transitions
